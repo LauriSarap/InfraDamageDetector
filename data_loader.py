b56d27f8f7e343bba486769d11fc2f5fc0f7a6e4
Lauri Sarap <lauri.aardla@gmail.com>
2023-11-05 11:37:23 +0200

    updates

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,22 @@
 import os
 
-if not os.path.exists("newimage"):
-    os.mkdir("newimage")
-newimage_directory = "newimage/"
-oldimage_directory = "oldimage/"
+
+def generate_output_image_name(base_name="output_image", extension=".tif"):
+    counter = 1
+    output_image = f"{base_name}_{counter}{extension}"
+
+    while os.path.exists(output_image):
+        counter += 1
+        output_image = f"{base_name}_{counter}{extension}"
+
+    return output_image
+
+
+def find_band_files(directory, bands):
+    files = {band: None for band in bands}
+    for filename in os.listdir(directory):
+        for band in bands:
+            if f"_B{band}_" in filename:
+                files[band] = os.path.join(directory, filename)
+                break  # Found the band file, move to the next one
+    return files
--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import subprocess
 from osgeo import gdal
 import os
+import data_loader
 
+# Settings
+x_shift = 0
+y_shift = 0
+
+gdal_calc_path = 'C:\\ProgramData\\anaconda3\\Scripts\\gdal_calc.py'
+output_image = data_loader.generate_output_image_name()
 
 newimage_directory = "newimage/"
 oldimage_directory = "oldimage/"
 
-
-def generate_output_image_name(base_name="output_image", extension=".tif"):
-    counter = 1
-    output_image = f"{base_name}_{counter}{extension}"
-
-    while os.path.exists(output_image):
-        counter += 1
-        output_image = f"{base_name}_{counter}{extension}"
-
-    return output_image
-
-
-output_image = generate_output_image_name()
-gdal_calc_path = 'C:\\ProgramData\\anaconda3\\Scripts\\gdal_calc.py'
+def convert_band(m20_b11, m10_b11):
+    image_band_conversion_cmd = [
+        'gdalwarp',
+        m20_b11,
+        m10_b11,
+        '-tr', '10', '10'
+    ]
+    subprocess.run(image_band_conversion_cmd)
 
 
 def shift_raster(input_file, output_file, x_shift, y_shift):
@@ -59,28 +60,10 @@ def shift_raster(input_file, output_file, x_shift, y_shift):
     out_dataset = None
 
 
-x_shift = 0
-y_shift = 0
-
-#shift_raster(new_b08, shifted_new_b08, x_shift, y_shift)
-shift_raster(old_b08, shifted_old_b08, x_shift, y_shift)
-#shift_raster(converted_new_b11, shifted_new_b11, x_shift, y_shift)
-shift_raster(converted_old_b11, shifted_old_b11, x_shift, y_shift)
-
-
-def find_band_files(directory, bands):
-    files = {band: None for band in bands}
-    for filename in os.listdir(directory):
-        for band in bands:
-            if f"_B{band}_" in filename:
-                files[band] = os.path.join(directory, filename)
-                break  # Found the band file, move to the next one
-    return files
-
-
 def process_images(new_images, old_images):
     for band in new_images:
-        if band == '11':
+        print("Converting band 11 to 10m resolution")
+        if band == '11' and new_images[band].endswith('_20m.jp2'):
             new_converted = new_images[band].replace('.jp2', '_10m.jp2')
             old_converted = old_images[band].replace('.jp2', '_10m.jp2')
             convert_band(new_images[band], new_converted)
@@ -89,22 +72,20 @@ def process_images(new_images, old_images):
             old_images[band] = old_converted
 
         # Shift rasters
-        shifted_new = new_images[band].replace('.jp2', '_shifted.tif')
+        print(f'Shifting old rasters by X:{x_shift} and Y:{y_shift}')
         shifted_old = old_images[band].replace('.jp2', '_shifted.tif')
-        shift_raster(new_images[band], shifted_new, x_shift, y_shift)
         shift_raster(old_images[band], shifted_old, x_shift, y_shift)
 
         # Update the dictionary with the shifted file paths
-        new_images[band] = shifted_new
         old_images[band] = shifted_old
 
     diff_calc_cmd = [
         'python',
         gdal_calc_path,
-        '-a', shifted_old_b08,
-        '-b', shifted_old_b11,
+        '-a', old_images['08'],
+        '-b', old_images['11'],
         '-c', new_images['08'],
-        '-d', converted_new_b11,
+        '-d', new_images['11'],
         '--outfile=' + output_image,
         '--calc="(b-a)/(b+a) - (d-c)/(d+c)"',
         '--overwrite'
@@ -114,19 +95,10 @@ def process_images(new_images, old_images):
     print("Running diff calc")
     subprocess.run(diff_calc_cmd)
 
-diff_calc_cmd = [
-    'python',
-    gdal_calc_path,
-    '-a', shifted_old_b08,
-    '-b', shifted_old_b11,
-    '-c', new_b08,
-    '-d', converted_new_b11,
-    '--outfile=' + output_image,
-    '--calc="(b-a)/(b+a) - (d-c)/(d+c)"',
-    '--overwrite'
-    #'--NoDataValue=0'
-]
 
-print("Running diff calc")
-subprocess.run(diff_calc_cmd)
+bands = ['03', '08', '11']
 
+new_images = data_loader.find_band_files(newimage_directory, bands)
+old_images = data_loader.find_band_files(oldimage_directory, bands)
+
+process_images(new_images, old_images)
